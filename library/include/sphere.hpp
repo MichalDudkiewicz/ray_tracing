c0140957442431374e87cb0263a4ec39e25da668
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2022-03-12 23:36:00 +0100

    sphere class

---
+++ library/include/sphere.hpp
@@ -0,0 +1,21 @@
+#include "vector.hpp"
+#include "ray.hpp"
+
+enum IntersectionCase {
+    MISS, HIT, INPRIM
+};
+
+class Sphere {
+public:
+    Sphere(const Vector& center, float radius = 0.0f);
+
+    const Vector& center() const;
+    float radius() const;
+
+    bool isOnSurface(const Vector& point) const;
+    IntersectionCase intersect(const Ray& ray, float& dist) const;
+
+private:
+    Vector mCenter;
+    float mRadius;
+};
---
+++ library/src/sphere.cpp
@@ -0,0 +1,44 @@
+#include "sphere.hpp"
+#include <cmath>
+
+Sphere::Sphere(const Vector &center, float radius) : mCenter(center), mRadius(radius) {
+}
+
+const Vector &Sphere::center() const {
+    return mCenter;
+}
+
+float Sphere::radius() const {
+    return mRadius;
+}
+
+bool Sphere::isOnSurface(const Vector &point) const {
+    const auto pointOriginDiff = point - mCenter;
+    return (pointOriginDiff.dotProduct(pointOriginDiff) - powf(mRadius, 2.0f)) == 0.0f;
+}
+
+IntersectionCase Sphere::intersect(const Ray &ray, float &dist) const {
+    Vector v = ray.origin() - mCenter;
+    float b = -v.dotProduct(ray.direction());
+    float det = (b * b) - v.dotProduct(v) + powf(mRadius, 2.0f);
+    IntersectionCase intersectionCase = MISS;
+    if (det > 0) {
+        det = sqrtf(det);
+        float i1 = b - det;
+        float i2 = b + det;
+        if (i2 > 0) {
+            if (i1 < 0) {
+                if (i2 < dist) {
+                    dist = i2;
+                    intersectionCase = INPRIM;
+                }
+            } else {
+                if (i1 < dist) {
+                    dist = i1;
+                    intersectionCase = HIT;
+                }
+            }
+        }
+    }
+    return intersectionCase;
+}
