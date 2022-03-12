5e37153831d329c8b533a44e5d6a678196751a15
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2022-03-12 22:22:06 +0100

    method calculating angle between vectors

--- library/include/vector_operators.hpp
+++ library/include/vector_operators.hpp
@@ -5,4 +5,5 @@
 #include <ostream>
 
 Vector operator*(float scalar, const Vector& vector);
-std::ostream& operator<<(std::ostream& os, const Vector& dt);
+std::ostream& operator<<(std::ostream& os, const Vector& vector);
+float angleBetween(const Vector& v1, const Vector& v2);
--- library/src/vector_operators.cpp
+++ library/src/vector_operators.cpp
@@ -1,5 +1,7 @@
 #include "vector_operators.hpp"
 
+#include <cmath>
+
 Vector operator*(const float scalar, const Vector& vector)
 {
     return {vector.x() * scalar, vector.y() * scalar, vector.z() * scalar};
@@ -9,4 +11,9 @@ std::ostream& operator<<(std::ostream& os, const Vector& vector)
 {
     os << "Vector[" << vector.x() << ',' << vector.y() << ',' << vector.z() << ']';
     return os;
+}
+
+float angleBetween(const Vector& v1, const Vector& v2)
+{
+    return asinf(v1.crossProduct(v2).length() / (v1.length() * v2.length()));
 }
