2d966d8d2bcb0271ddec53a314659ad3f360a214
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2022-03-19 15:29:35 +0100

    light intensity class

---
+++ library/include/light_intensity.hpp
@@ -0,0 +1,27 @@
+#include <ostream>
+
+class LightIntensity
+{
+public:
+    LightIntensity(float r = 0.0, float g = 0.0, float b = 0.0);
+
+    float red() const;
+    float green() const;
+    float blue() const;
+
+    void operator()(float r, float g = 0.0, float b = 0.0);
+
+    void add(float r, float g, float b);
+
+    LightIntensity operator+(const LightIntensity &li) const;
+    LightIntensity operator-(const LightIntensity &li) const;
+    LightIntensity operator/(float num) const;
+    LightIntensity operator*(float num) const;
+
+    friend LightIntensity operator*(float num, LightIntensity &li);
+    friend std::ostream& operator<<(std::ostream& str,
+                                    const LightIntensity &li);
+
+private:
+    float mR, mG, mB;
+};
---
+++ library/src/light_intensity.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include "light_intensity.hpp"
+
+LightIntensity::LightIntensity(float r, float g, float b) : mR(r), mG(g), mB(b) {
+    mR = std::clamp(mR, 0.0f, 1.0f);
+    mG = std::clamp(mG, 0.0f, 1.0f);
+    mB = std::clamp(mB, 0.0f, 1.0f);
+}
+
+float LightIntensity::red() const {
+    return mR;
+}
+
+float LightIntensity::green() const {
+    return mG;
+}
+
+float LightIntensity::blue() const {
+    return mB;
+}
+
+void LightIntensity::operator()(float r, float g, float b) {
+    mR = r;
+    mG = g;
+    mB = b;
+    mR = std::clamp(mR, 0.0f, 1.0f);
+    mG = std::clamp(mG, 0.0f, 1.0f);
+    mB = std::clamp(mB, 0.0f, 1.0f);
+}
+
+void LightIntensity::add(float r, float g, float b) {
+    mR += r;
+    mG += g;
+    mB += b;
+    mR = std::clamp(mR, 0.0f, 1.0f);
+    mG = std::clamp(mG, 0.0f, 1.0f);
+    mB = std::clamp(mB, 0.0f, 1.0f);
+}
+
+LightIntensity LightIntensity::operator+(const LightIntensity &li) const {
+    return {mR + li.mR, mG + li.mG, mB + li.mB};
+}
+
+LightIntensity LightIntensity::operator-(const LightIntensity &li) const {
+    return {mR - li.mR, mG - li.mG, mB - li.mB};
+}
+
+LightIntensity LightIntensity::operator/(float num) const {
+    if (num == 0.0f)
+    {
+        throw std::runtime_error("cannot divide by zero");
+    }
+    return {mR / num, mG / num, mB / num};
+}
+
+LightIntensity LightIntensity::operator*(float num) const {
+    return {mR * num, mG * num, mB * num};
+}
+
+LightIntensity operator*(float num, LightIntensity &li) {
+    return {li.mR * num, li.mG * num, li.mB * num};
+}
+
+std::ostream& operator<<(std::ostream &str, const LightIntensity &li) {
+    str << "LightIntensity[" << li.mR << ',' << li.mG << ',' << li.mB << ']';
+    return str;
+}
