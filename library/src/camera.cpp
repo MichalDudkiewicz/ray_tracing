c07b1118a98eecaee7e85771b70c1bf5472688b4
Michal Dudkiewicz <michal.dudkiewicz@tomtom.com>
2022-05-17 09:52:20 +0200

    optimizing

--- library/src/camera.cpp
+++ library/src/camera.cpp
@@ -181,150 +181,132 @@ RGBApixel Camera::getColorByPosition(const Vector& position) const {
 LightIntensity Camera::traceRay(const Ray& ray, int reflectedRayCounter) const
 {
     LightIntensity accumulatedLightIntensity(0.5, 0.5, 0.5);
-    float minDistance = ray.distance();
-    for (const auto& mesh : mScene.meshes())
-    {
-        if (mesh.hasBoundingBox() && !mesh.intersectsBoundingBox(ray))
-        {
-            continue;
-        }
-
-        const auto intersection = mesh.intersection(ray);
-        if (intersection.has_value())
-        {
-            const auto intersectionPoint = std::get<0>(intersection.value());
-            const float newDistance = (intersectionPoint - ray.origin()).length();
-            if (newDistance > minDistance)
-            {
+    if (reflectedRayCounter < kMaxReflectedRaysNumber) {
+        float minDistance = ray.distance();
+        for (const auto &mesh: mScene.meshes()) {
+            if (mesh.hasBoundingBox() && !mesh.intersectsBoundingBox(ray)) {
                 continue;
             }
-            else
-            {
-                minDistance = newDistance;
-            }
 
-            const auto texture = mesh.material().texture();
-            LightIntensity textureLightIntensity(1.0f, 1.0f, 1.0f);
-            if (texture)
-            {
-                const auto primitiveHit = std::get<2>(intersection.value());
-                //TODO: how to handle any mesh and unwrapping any geometry?
-                const auto sphere = std::dynamic_pointer_cast<Sphere>(primitiveHit);
-                const auto plane = std::dynamic_pointer_cast<Plane>(primitiveHit);
-                if (sphere)
-                {
-                    const auto localCoordsIntersection = intersectionPoint - sphere->center();
-                    auto phi = atanf(localCoordsIntersection.x()/localCoordsIntersection.z());
-                    if (phi < 0.0f)
-                    {
-                        phi += 2 * M_PI;
-                    }
-                    // TODO: what if radius is bigger than 1.0f?
-                    // http://raytracerchallenge.com/bonus/texture-mapping.html?fbclid=IwAR03ywQ6spI8EHgCBHqTEC04idLVRhSY7m_eAlEozc5tN8Ug8V2M6zxLH1Q
-                    const auto theta = acosf(localCoordsIntersection.y());
-                    const float u = phi / (2 * M_PI);
-                    const float v = 1.0f - theta / M_PI;
-                    textureLightIntensity = texture->color(u, v);
+            const auto intersection = mesh.intersection(ray);
+            if (intersection.has_value()) {
+                const auto intersectionPoint = std::get<0>(intersection.value());
+                const float newDistance = (intersectionPoint - ray.origin()).length();
+                if (newDistance > minDistance) {
+                    continue;
+                } else {
+                    minDistance = newDistance;
                 }
-                else if (plane)
-                {
-                    // TODO: how to handle normalising plane process correctly?
-                    auto localCoordsIntersection = intersectionPoint - plane->point();
-                    if (plane->normal(intersectionPoint).x() != 1.0f)
-                    {
-                        localCoordsIntersection.setX(localCoordsIntersection.x()/std::abs((plane->lowerLeftCorner().x() - plane->upperRightCorner().x())));
-                    }
-                    if (plane->normal(intersectionPoint).y() != 1.0f)
-                    {
-                        localCoordsIntersection.setY(localCoordsIntersection.y()/std::abs((plane->lowerLeftCorner().y() - plane->upperRightCorner().y())));
-                    }
-                    if (plane->normal(intersectionPoint).z() != 1.0f)
-                    {
-                        localCoordsIntersection.setZ(localCoordsIntersection.z()/std::abs((plane->lowerLeftCorner().z() - plane->upperRightCorner().z())));
-                    }
 
-                    // TODO: how to handle rotated plane, so that y is 0 and z is not?
-                    const float u = ((localCoordsIntersection.y()) + 1.0f) / 2.0f;
-                    const float v = (localCoordsIntersection.x() + 1.0f) / 2.0f;
+                const auto texture = mesh.material().texture();
+                LightIntensity textureLightIntensity(1.0f, 1.0f, 1.0f);
+                if (texture) {
+                    const auto primitiveHit = std::get<2>(intersection.value());
+                    //TODO: how to handle any mesh and unwrapping any geometry?
+                    const auto sphere = std::dynamic_pointer_cast<Sphere>(primitiveHit);
+                    const auto plane = std::dynamic_pointer_cast<Plane>(primitiveHit);
+                    if (sphere) {
+                        const auto localCoordsIntersection = intersectionPoint - sphere->center();
+                        auto phi = atanf(localCoordsIntersection.x() / localCoordsIntersection.z());
+                        if (phi < 0.0f) {
+                            phi += 2 * M_PI;
+                        }
+                        // TODO: what if radius is bigger than 1.0f?
+                        // http://raytracerchallenge.com/bonus/texture-mapping.html?fbclid=IwAR03ywQ6spI8EHgCBHqTEC04idLVRhSY7m_eAlEozc5tN8Ug8V2M6zxLH1Q
+                        const auto theta = acosf(localCoordsIntersection.y());
+                        const float u = phi / (2 * M_PI);
+                        const float v = 1.0f - theta / M_PI;
+                        textureLightIntensity = texture->color(u, v);
+                    } else if (plane) {
+                        // TODO: how to handle normalising plane process correctly?
+                        auto localCoordsIntersection = intersectionPoint - plane->point();
+                        if (plane->normal(intersectionPoint).x() != 1.0f) {
+                            localCoordsIntersection.setX(localCoordsIntersection.x() / std::abs(
+                                    (plane->lowerLeftCorner().x() - plane->upperRightCorner().x())));
+                        }
+                        if (plane->normal(intersectionPoint).y() != 1.0f) {
+                            localCoordsIntersection.setY(localCoordsIntersection.y() / std::abs(
+                                    (plane->lowerLeftCorner().y() - plane->upperRightCorner().y())));
+                        }
+                        if (plane->normal(intersectionPoint).z() != 1.0f) {
+                            localCoordsIntersection.setZ(localCoordsIntersection.z() / std::abs(
+                                    (plane->lowerLeftCorner().z() - plane->upperRightCorner().z())));
+                        }
 
-                    textureLightIntensity = texture->color(u, v);
+                        // TODO: how to handle rotated plane, so that y is 0 and z is not?
+                        const float u = ((localCoordsIntersection.y()) + 1.0f) / 2.0f;
+                        const float v = (localCoordsIntersection.x() + 1.0f) / 2.0f;
+
+                        textureLightIntensity = texture->color(u, v);
+                    }
                 }
-            }
-            const auto ambientLightIntensity = mesh.material().ambientLight() * textureLightIntensity;
+                const auto ambientLightIntensity = mesh.material().ambientLight() * textureLightIntensity;
 
-            accumulatedLightIntensity = ambientLightIntensity;
+                accumulatedLightIntensity = ambientLightIntensity;
 
-            IntersectionInfo intersectionInfo(mesh.material(), intersectionPoint, std::get<1>(intersection.value()));
+                IntersectionInfo intersectionInfo(mesh.material(), intersectionPoint,
+                                                  std::get<1>(intersection.value()));
 
-            for (const auto& light : mScene.lights()) 
-            {
-                auto lightDir = light->lightDirection(intersectionInfo.position()).normalize();
-                const Vector beforeIntersectionPoint = intersectionInfo.position() + 0.1 * lightDir;
-                // TODO: shadowRay distance for directional light should be calculated dirrefently than based on light position.
-                //  most probably it should be big number, in theory close to infinity
-                const Ray shadowRay(beforeIntersectionPoint, lightDir, light->lightDirection(beforeIntersectionPoint).length() - 0.01f);
-                bool isInShadow = false;
-                for (const auto& mesh2 : mScene.meshes())
-                {
-                    for (const auto& primitive2 : mesh2.primitives())
-                    {
-                        const auto intersection2 = primitive2->intersection(shadowRay);
-                        if (intersection2.has_value())
-                        {
-                            isInShadow = true;
-                            break;
+                for (const auto &light: mScene.lights()) {
+                    auto lightDir = light->lightDirection(intersectionInfo.position()).normalize();
+                    const Vector beforeIntersectionPoint = intersectionInfo.position() + 0.1 * lightDir;
+                    // TODO: shadowRay distance for directional light should be calculated dirrefently than based on light position.
+                    //  most probably it should be big number, in theory close to infinity
+                    const Ray shadowRay(beforeIntersectionPoint, lightDir,
+                                        light->lightDirection(beforeIntersectionPoint).length() - 0.01f);
+                    bool isInShadow = false;
+                    for (const auto &mesh2: mScene.meshes()) {
+                        for (const auto &primitive2: mesh2.primitives()) {
+                            const auto intersection2 = primitive2->intersection(shadowRay);
+                            if (intersection2.has_value()) {
+                                isInShadow = true;
+                                break;
+                            }
                         }
                     }
-                }
-                if (!isInShadow || mesh.material().refractionFactor() > 1.0f || mesh.material().isMirror())
-                {
-                    const auto diffuseLightIntensity = light->diffuse(intersectionInfo) * textureLightIntensity;
-                    const auto specularLightIntensity = light->specular(intersectionInfo, ray);
-                    accumulatedLightIntensity += diffuseLightIntensity + specularLightIntensity;
+                    if (!isInShadow || mesh.material().refractionFactor() > 1.0f || mesh.material().isMirror()) {
+                        const auto diffuseLightIntensity = light->diffuse(intersectionInfo) * textureLightIntensity;
+                        const auto specularLightIntensity = light->specular(intersectionInfo, ray);
+                        accumulatedLightIntensity += diffuseLightIntensity + specularLightIntensity;
 
-                    if (reflectedRayCounter < kMaxReflectedRaysNumber) {
                         if (mesh.material().isMirror()) {
                             const Vector V = ray.direction().normalize();
-                            const Vector& N = intersectionInfo.normal();
+                            const Vector &N = intersectionInfo.normal();
                             const Vector R = V - (N * N.dotProduct(V) * 2.0f);
 
                             const Vector beforeIntersectionVPoint = intersectionInfo.position() + 0.1 * N;
                             Ray reflectedRay(beforeIntersectionVPoint, R, 1000.0f);
                             const auto reflectedLightIntensity = traceRay(reflectedRay, reflectedRayCounter + 1);
                             accumulatedLightIntensity += reflectedLightIntensity;
-                        }
-                        else if (mesh.material().refractionFactor() > 1.0f) {
+                        } else if (mesh.material().refractionFactor() > 1.0f) {
                             const Vector d = ray.direction().normalize();
-                            const Vector& n = intersectionInfo.normal();
+                            const Vector &n = intersectionInfo.normal();
                             const auto lambdaT = mesh.material().refractionFactor();
                             Vector t = (d - n * (d.dotProduct(n))) / lambdaT -
-                                n * sqrtf(1 - (1 - powf(d.dotProduct(n), 2.0f)) / powf(lambdaT, 2.0f));
+                                       n * sqrtf(1 - (1 - powf(d.dotProduct(n), 2.0f)) / powf(lambdaT, 2.0f));
                             t = t.normalize();
                             const Vector innerIntersection = intersectionPoint - 0.01 * n;
                             const Ray innerRay(innerIntersection, t, 1000.0f);
                             const auto innerSecondIntersection = mesh.intersection(innerRay);
                             if (innerSecondIntersection.has_value()) {
-                                const Vector& d2 = t;
+                                const Vector &d2 = t;
                                 Vector n2 = std::get<1>(innerSecondIntersection.value());
                                 n2.negate();
                                 Vector t2 = lambdaT * (d2 - n2 * (d2.dotProduct(n2))) -
-                                    n2 * sqrtf(1 - powf(lambdaT, 2.0f) * (1 - powf(d2.dotProduct(n2), 2.0f)));
+                                            n2 * sqrtf(1 - powf(lambdaT, 2.0f) * (1 - powf(d2.dotProduct(n2), 2.0f)));
                                 t2 = t2.normalize();
                                 const auto innerSecondIntersectionPoint =
-                                    std::get<0>(innerSecondIntersection.value()) - 0.01 * n2;
+                                        std::get<0>(innerSecondIntersection.value()) - 0.01 * n2;
                                 const Ray refractedRay(innerSecondIntersectionPoint, t2, 1000.0f);
                                 const auto refractedLightIntensity = traceRay(refractedRay, reflectedRayCounter + 1);
                                 accumulatedLightIntensity += refractedLightIntensity;
                             }
                         }
+                    } else {
+                        accumulatedLightIntensity -= mesh.material().shadowLight();
                     }
                 }
-                else
-                {
-                    accumulatedLightIntensity -= mesh.material().shadowLight();
-                }
             }
-            
         }
     }
     return accumulatedLightIntensity;
